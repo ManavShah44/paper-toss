3d3d29577430bf1f6eb25151f57364056e19ee52
ManavShah44 <74656329+ManavShah44@users.noreply.github.com>
2020-12-24 16:36:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var bin1,bin2,bin3,ball,ground
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(1000, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	bin1 = new Bin(550,600,20,150);
+	bin2=new Bin(675,650,250,25)
+	bin3=new Bin(800,600,25,150)
+	ball=new Ball(250,250,20,20)
+	ground=new Ground(250,250,20,20)
+	   
+	
+
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  bin1.display()
+  bin2.display()
+  bin3.display()
+  ball.display()
+  ground.display()
+  
+}
+
+
+
